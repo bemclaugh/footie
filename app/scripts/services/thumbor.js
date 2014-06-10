2e40e72168993ea4ef908b43722238bad1adadf7
Brian McLaughlin <bemclaugh@gmail.com>
2014-06-09 22:33:38 -1000

    added beginning thumbor

--- app/scripts/services/thumbor.js
+++ app/scripts/services/thumbor.js
@@ -2,19 +2,25 @@
 
 angular.module('footieApp', [])
 .factory('ThumborService', function($http) {
+  /*
   var getThumborServer = function() {
     return 'http://localhost';
   };
-  var test_url = 'https://picasaweb.google.com/data/feed/api/all?q=%22hawaii%22&kind=photo&access=all&alt=rss';
+  */
+  var testUrl = 'https://picasaweb.google.com/data/feed/api/all?q=%22hawaii%22&kind=photo&access=all&alt=rss';
   var request = {
-    url: test_url;
+    url: testUrl,
+    image: ''
   };
+  /*
   var buildUrl = function(method='', image='') {
     return getThumborServer() + method + image;
   };
+  */
   var shrink = function(image) {
-    //request['url'] = buildUrl();
-    return $http.get(request).then(function(result) {
+    //request.url = buildUrl();
+    request.image = image;
+    return $http.get(request).then(function(response) {
       return response.data;
     });
   };
