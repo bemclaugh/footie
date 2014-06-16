cb318e29ad6795e727da84cb8f41e690250a33ce
Brian McLaughlin <bemclaugh@gmail.com>
2014-06-15 21:53:00 -1000

    removed unused about file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,10 +17,6 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
       .when('/shrink/', {
         templateUrl: 'views/images.html',
         controller: 'ImageCtrl'
